df2a9c07f24a35a12709fe751887af874dace2aa
BruunoMM <brunommarinho@hotmail.com>
2017-09-05 22:39:57 -0300

    Delete imprimeAluno.c

--- imprimeAluno.c
+++
@@ -1,7 +0,0 @@
-/*Função recebe um ponteiro pra aluno e imprime todos os seus dados*/
-int imprimeAluno (Aluno *a){
-	if (a == NULL)
-		return ();
-	printf ("Nome: %s \nMatricula: %d \nCPF: %ld \nTelefone: %d \nData de Nascimento: %hd/%hd/%hd \nEndereco: %s, %s, %s, %s, %s\n", a->nome, a->mat, a->cpf, a->telefone, a->nasc.dia, a->nasc.mes, a->nasc.ano, a->end.estado, a->end.cidade, a->end.bairro, a->end.rua, a->end.comp);
-	return ();
-}
