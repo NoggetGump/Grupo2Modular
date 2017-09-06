7e62b9c5a9ca266cf0125fc8fa2d0f8fb315fcd4
flaviofvaz <flaviofrancovaz@gmail.com>
2017-09-05 22:28:08 -0300

    Create imprimeAluno.c

---
+++ imprimeAluno.c
@@ -0,0 +1,6 @@
+int imprimeAluno (Aluno *a){
+	if (a == NULL)
+		return ();
+	printf ("Nome: %s \nMatricula: %d \nCPF: %ld \nTelefone: %d \nData de Nascimento: %hd/%hd/%hd \nEndereco: %s, %s, %s, %s, %s\n", a->nome, a->mat, a->cpf, a->telefone, a->nasc.dia, a->nasc.mes, a->nasc.ano, a->end.estado, a->end.cidade, a->end.bairro, a->end.rua, a->end.comp);
+	return ();
+}
