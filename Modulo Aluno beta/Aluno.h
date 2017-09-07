97515266f7abbaaa129d106b33db4e08ffe82a64
BruunoMM <brunommarinho@hotmail.com>
2017-09-07 02:42:33 -0300

    Update (versão 0.5)

--- Modulo Aluno beta/Aluno.h
+++ Modulo Aluno beta/Aluno.h
@@ -15,8 +15,6 @@ typedef struct endereco {
 	char comp[32];
 } Endereco;
 
-typedef struct aluno Aluno;
-
 typedef enum {
 	ALN_CondRetOK,	// Executou corretamente
 	ALN_CondRetAlunoNaoExiste,	// O aluno não existe 
@@ -24,36 +22,30 @@ typedef enum {
 	ALN_CondRetFaltouMemoria	// Faltou memória ao alocar dados
 } ALN_tpCondRet;
 
-
+typedef struct aluno Aluno;
 
 // Função para criar um aluno. Recebe um ponteiro para Aluno e retorna o aluno criado com as informações passadas por parâmetros.
-ALN_tpCondRet CriaAluno(Aluno *a, char *nome, int mat, int cpf, int telefone, Data *nasc, Endereco* end);
+ALN_tpCondRet ALU_CriaAluno(Aluno **a, char *nome, int mat, long cpf, int telefone, Data *nasc, Endereco* end);
 
 // Função para deletar um aluno. Recebe o endereço de memória de um aluno e o deleta (apaga da memória).*/
-ALN_tpCondRet deletaAluno(Aluno* a);
+ALN_tpCondRet ALU_deletaAluno(Aluno* a);
 
 // Altera os dados de um aluno, caso eles sejam diferente de NULL / 0 (Zero). Caso um dado não necessite ser alterado, passar 0 / NULL como parâmetro.
-ALN_tpCondRet AlteraDados(Aluno *aluno, char *nome, int mat, int cpf, int telefone, Data *nasc, Endereco* end);
+ALN_tpCondRet ALU_AlteraDados(Aluno *a, char *nome, int mat, long cpf, int telefone, Data *nasc, Endereco* end);
 
 // Função para pegar a matrícula de um aluno. Recebe o endereço de memória (ponteiro) de um aluno e devolve sua matrícula por referência.
-ALN_tpCondRet GetMat(Aluno *a, int *mat);
+ALN_tpCondRet ALU_GetMat(Aluno *a, unsigned int *mat);
 
 // Função para pegar o nome de um aluno. Recebe um ponteiro para Aluno e devolve seu nome por referência.
-ALN_tpCondRet GetNome(Aluno *a, char* nome);
+ALN_tpCondRet ALU_GetNome(Aluno *a, char* nome);
 
 // Função para solicitar dados de alunos através da saída DOS. Devolve as entradas como parâmetros.
-ALN_tpCondRet SolicitaDados(char *nome, int *mat, int *cpf, int *telefone, Data *nasc, Endereco* end);
+ALN_tpCondRet ALU_SolicitaDados(char *nome, int *mat, long *cpf, int *telefone, Data *nasc, Endereco* end);
 
 // Função que copia os dados do aluno
-ALN_tpCondRet GetAll(Aluno *a, char *nome, int *mat, int *cpf, int *tel, Data *nasc, Endereco *endereco);
+ALN_tpCondRet ALU_GetAll(Aluno *a, char *nome, int *mat, long *cpf, int *tel, Data *nasc, Endereco *endereco);
 
 // Função recebe um ponteiro pra aluno e imprime todos os seus dados
-ALN_tpCondRet imprimeAluno(Aluno *a);
-
-// Função interna para validar uma data.
-// Considera a data atual do sistema como data limite.
-// Retorna 1 se a data for válida e 0 caso contrário.
-// Valida verificando o número de dias por mês, p. ex. O dia 31 de setembro não é válido. */
-int ValidaData(Data* data);
+ALN_tpCondRet ALU_imprimeAluno(Aluno *a);
 
 #endif
