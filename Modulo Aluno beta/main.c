f528690455c1468b93655cf96779f29f998cacc5
BruunoMM <brunommarinho@hotmail.com>
2017-09-07 18:32:48 -0300

    Update main.c

--- Modulo Aluno beta/main.c
+++ Modulo Aluno beta/main.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	Aluno **a = NULL;
+	Aluno *a = NULL;
 	Data *nascTest = (Data*) malloc(sizeof(Data));
 	Endereco *endTest = (Endereco*) malloc(sizeof(Endereco));
 	int mat, tel = 0;
-	long cpf;
+	// long cpf;
 	char nome[81];
 	
 	// Testa funções CriaAluno, GetMat, GetNome, AlteraDados
@@ -26,24 +26,26 @@ int main(void)
 	strcpy(endTest->rua, "Rua Marques de Sao Vicente");
 	strcpy(endTest->comp, "225");
 
-	ALU_CriaAluno(&a,"Pessoa Souza",1612220,14020210100,991749212,nascTest,endTest);
-	/*
+	ALU_CriaAluno(&a,"Pessoa Souza",1612220,(long)14020210100,991749212,nascTest,endTest);
+	
 	ALU_GetMat(a, &mat);
 	ALU_GetNome(a,nome);
-	printf("Matricula adicionada: %u\n", mat);
-	printf("Nome: %s\n",nome);
+	ALU_imprimeAluno(a);
+	//printf("Matricula Inalterada: %u\n", mat);
+	//printf("Nome Inalterado: %s\n",nome);
+	printf("--------- ALTERACAO ---------\n");
 	ALU_AlteraDados(a,"Aluno da Silva", 1610001,0,0,NULL,NULL);
 	ALU_GetMat(a, &mat);
 	ALU_GetNome(a, nome);
-	printf("Matricula adicionada: %u\n", mat);
-	printf("Nome: %s\n", nome);
-	*/
+	ALU_imprimeAluno(a);
+	//printf("Matricula Alterada: %u\n", mat);
+	//printf("Nome Alterado: %s\n", nome);
 
 	// Testa a Função Solicita Dados, em seguida cria um aluno com os dados inseridos
 	// e imprime os dados do aluno na tela.
 	//ALU_SolicitaDados(nome,&mat,&cpf,&tel,nascTest,endTest);
 	//ALU_CriaAluno(&a, nome, mat, cpf, tel, nascTest, endTest);
-	ALU_imprimeAluno(a);
+	//ALU_imprimeAluno(a);
 	//ALU_GetMat(a, &mat);
 	//ALU_GetNome(a, nome);
 	//printf("Matricula adicionada: %u\n", mat);
