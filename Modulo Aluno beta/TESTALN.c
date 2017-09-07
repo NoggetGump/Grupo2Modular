e95c13ce82315bf75316242cdac14d0a2b6f2c4d
joo2118 <31517422+joo2118@users.noreply.github.com>
2017-09-07 12:15:24 -0300

    Update TESTALN.c

--- Modulo Aluno beta/TESTALN.c
+++ Modulo Aluno beta/TESTALN.c
@@ -20,9 +20,12 @@
 #define     INS_DADO_CMD        "=insdado"
 #define     IMP_ALUN_CMD        "=impaluno"
 
-#define DIM_VT_ALUNO   10
+#define DIM_VT   10
+#define  DIM_STRING  250
 
-Aluno vtAlunos[ DIM_VT_ALUNO ] ;
+Aluno vtAlunos[ DIM_VT] ;
+Data vtDatas[ DIM_VT];
+Endereco vtEnds[ DIM_VT];
 
 
    TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
@@ -34,45 +37,55 @@ Aluno vtAlunos[ DIM_VT_ALUNO ] ;
 
       char ValorEsperado = '?'  ;
       char ValorObtido   = '!'  ;
+	  char StringNome[ DIM_STRING ];
+	  char StringObtido[DIM_STRING];
 	  int MatEsperada = '-1';
 	  int MatObtida = '-1' ;
 	  int indxaluno ='-1';
+	  int nome;
+	  int cpf;
+	  int telefone;
+	  int indxdata;
+	  int indxendereco;
 	
       int  NumLidos = -1 ;
 
       TST_tpCondRet Ret ;
 
-      /* Testar ALN Criar aluno -AINDA INCOMPLETA*/
+      /* Testar ALU Criar aluno*/
 
          if ( strcmp( ComandoTeste , CRIAR_ALN_CMD ) == 0 )
          {
 
-            NumLidos = LER_LerParametros( "ci" ,&indxaluno,
+            NumLidos = LER_LerParametros( "isiiiiii" ,&indxaluno,StringNome,&MatEsperada,&cpf,&telefone,&indxdata,&indxendereco,
                                &CondRetEsperada ) ;
-            if ( NumLidos != 1 )
+            if ( NumLidos != 8 )
             {
                return TST_CondRetParm ;
-            } /* if */
-
-            CondRetObtido = ALN_CriarAluno( ) ;
+            } 
+			/* testando se a matricula é valida, isto é, tem 7 números */
+			if(MatEsperada<1000000 || MatEsperada>9999999){
+				return TST_CondRetErro;
+			}
+            CondRetObtido = ALU_CriaAluno(vtAlunos[indxaluno],StringNome,MatEsperada,cpf,telefone,vtDatas[indxdata],vtEnds[indxendereco]);
 
             return TST_CompararInt( CondRetEsperada , CondRetObtido ,
                                     "Retorno errado ao criar Aluno." );
 
-         } /* fim ativa: Testar ARV Criar árvore */
+         }
 
-      /* Testar ALN pegar matrícula do aluno */
+      /* Testar ALU pegar matrícula do aluno */
 
          else if ( strcmp( ComandoTeste , GET_MAT_CMD ) == 0 )
          {
 			 /*indxaluno é o index do aluno declarado no script de teste(exemplo:criar aluno0, criar aluno1, a função lerparametros pega esse 0 e 1,que são indices para um vetor de alunos criado nesse módulo para teste */
-            NumLidos = LER_LerParametros( "isi" ,&indxaluno,&MatEsperada,&CondRetEsperada ) ;
+            NumLidos = LER_LerParametros( "iii" ,&indxaluno,&MatEsperada,&CondRetEsperada ) ;
             if ( NumLidos != 3 )
             {
                return TST_CondRetParm ;
             } /* if */
 
-            CondRetObtido = GetMat(vtAlunos[indxaluno], &MatObtida);
+            CondRetObtido = ALU_GetMat(vtAlunos[indxaluno], &MatObtida);
 			Ret = TST_CompararInt(CondRetEsperada,CondRetObtido,"Retorno errado");
 			if(Ret != TST_CondRetOK){
 				return Ret;
@@ -83,58 +96,58 @@ Aluno vtAlunos[ DIM_VT_ALUNO ] ;
 
          }
 
-      /* Testar ALN pegar nome do aluno */
+      /* Testar ALU pegar nome do aluno */
 
          else if ( strcmp( ComandoTeste , GET_NOME_CMD ) == 0 )
          {
-			 /*nesse caso abaixo ValorEsperado = nome do aluno digitado no script de teste */
-            NumLidos = LER_LerParametros( "isi" ,&indxaluno,&ValorEsperado,&CondRetEsperada );
+			 /*nesse caso abaixo StringNome = nome do aluno digitado no script de teste */
+            NumLidos = LER_LerParametros( "isi" ,&indxaluno,StringNome,&CondRetEsperada );
             if ( NumLidos != 3 )
             {
                return TST_CondRetParm ;
-            } /* if */
+            }
 
-            CondRetObtido = GetNome(vtAlunos[indxaluno], &ValorObtido);
+            CondRetObtido = ALU_GetNome(vtAlunos[indxaluno],StringObtido);
 			Ret = TST_CompararInt(CondRetEsperada,CondRetObtido,"Retorno errado");
 			if(Ret != TST_CondRetOK){
 				return Ret;
 			}
 
-            return TST_CompararChar( ValorEsperado,ValorObtido,
+            return TST_CompararString( StringNome,StringObtido,
                                     "Nome errado." );
-
          }
 
       /* Testar deletar aluno */
 
     else if ( strcmp( ComandoTeste , DEL_ALUN_CMD ) == 0 )
          {
-			NumLidos = LER_LerParametros( "i", &indxaluno ) ;
-            if ( NumLidos != 1 )
+			NumLidos = LER_LerParametros( "ii", &indxaluno,&CondRetEsperada) ;
+            if ( NumLidos != 2 )
             {
                return TST_CondRetParm ;
-            } /* if */
-             deletaAluno( vtAlunos[ indxaluno ]) ;
-            vtAlunos[ inxaluno] = NULL ;
-            return TST_CondRetOK ;
+            }
+            CondRetObtido=ALU_deletaAluno( vtAlunos[ indxaluno ]) ;
+            vtAlunos[ indxaluno] = NULL ;
+            return TST_CompararInt(CondRetEsperada,CondRetObtido,"Retorno errado");
          }
 		 /* Testar imprimir aluno */
 		   else if ( strcmp( ComandoTeste ,  IMP_ALUN_CMD) == 0 )
          {
-			NumLidos = LER_LerParametros( "ci" ,&indxaluno,
+			NumLidos = LER_LerParametros( "ii" ,&indxaluno,
                                &CondRetEsperada ) ;
             if ( NumLidos != 2 )
             {
                return TST_CondRetParm ;
-            } /* if */
-            CondRetObtido = imprimeAluno( vtAlunos[ indxaluno]) ;
-            return TST_CompararInt(condRetEsperada,CondRetObtida,"Retorno Errado");
-
+            }
+            CondRetObtido = ALU_imprimeAluno( vtAlunos[ indxaluno]) ;
+            return TST_CompararInt(CondRetEsperada,CondRetObtida,"Retorno Errado");
          }
 
       return TST_CondRetNaoConhec ;
 
    }
+
+      
 
       
 
