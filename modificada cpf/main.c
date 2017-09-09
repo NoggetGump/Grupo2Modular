04d4c4c5e1c462366d9c09218c25043fe8dd308c
MzBr <matheuszeitune2@gmail.com>
2017-09-09 11:32:50 -0300

    CPF fix

--- modificada cpf/main.c
+++ modificada cpf/main.c
@@ -6,17 +6,17 @@
 
 int main(void)
 {
+	char nomePessoa[81];
 	Aluno *a = NULL;
 	Data *nascTest = (Data*) malloc(sizeof(Data));
 	Endereco *endTest = (Endereco*) malloc(sizeof(Endereco));
 	unsigned int mat, tel, t = 0;
 	CPF *cpfTest = (CPF*) malloc(sizeof(CPF));
 	cpfTest->dig1= 123;
-	cpfTest->dig1= 456;
-	cpfTest->dig1= 789;
+	cpfTest->dig2= 456;
+	cpfTest->dig3= 789;
 	cpfTest->cod = 00;
-	//unsigned long cpf = 10990100200;
-	char nome[81];
+	//
 	
 	// Testa funções CriaAluno, GetMat, GetNome, AlteraDados
 	
@@ -33,17 +33,17 @@ int main(void)
 	ALU_CriaAluno(&a,"Pessoa Souza",1612220,cpfTest,991749212,nascTest,endTest);
 	
 	ALU_GetMat(a, &mat);
-	ALU_GetNome(a, nome);
+	ALU_GetNome(a, nomePessoa);
 	ALU_imprimeAluno(a);
 	printf("Matricula Inalterada: %u\n", mat);
-	printf("Nome Inalterado: %s\n",nome);
+	printf("Nome Inalterado: %s\n",nomePessoa);
 	printf("--------- ALTERACAO ---------\n");
 	ALU_AlteraDados(a,"Aluno da Silva", 1610001,0,0,NULL,NULL);
 	ALU_GetMat(a, &mat);
-	ALU_GetNome(a, nome);
+	ALU_GetNome(a, nomePessoa);
 	ALU_imprimeAluno(a);
 	printf("Matricula Alterada: %u\n", mat);
-	printf("Nome Alterado: %s\n", nome);
+	printf("Nome Alterado: %s\n", nomePessoa);
 
 	 //Testa a Função Solicita Dados, em seguida cria um aluno com os dados inseridos
 	// e imprime os dados do aluno na tela.
@@ -55,10 +55,10 @@ int main(void)
 	printf("Matricula adicionada: %u\n", mat);
 	printf("Nome: %s\n", nome);*/
 	//Testa função GetAll e imprime os dados obtidos na tela.
-	ALU_GetAll(a, nome, &mat, cpfTest, &tel, nascTest, endTest);
+	ALU_GetAll(a, nomePessoa, &mat, cpfTest, &tel, nascTest, endTest);
 	printf ("-------------------------Dados da GetAll----------------------------\n");
 	
-	printf ("Nome do Aluno: %s\n", nome);
+	printf ("Nome do Aluno: %s\n", nomePessoa);
 	printf ("Matricula: %d\n", mat);
 	printf ("CPF: %hd.%hd.%hd-%hd\n", cpfTest->dig1, cpfTest->dig2,cpfTest->dig3, cpfTest->cod);
 	printf ("Telefone: %d\n", tel);
