2688bd9002f814a2bec892b1e6f21e7c64c9d7e1
flaviofvaz <flaviofrancovaz@gmail.com>
2017-09-11 00:59:13 -0300

    Update Aluno.c

--- Aluno Arcabouco/ModA/Aluno.c
+++ Aluno Arcabouco/ModA/Aluno.c
@@ -144,7 +144,7 @@ ALN_tpCondRet ALU_imprimeAluno(Aluno *a) {
 	printf("Matricula: %d\n", a->mat);
 	printf("CPF:  %03d.%03d.%03d-%02d\n", a->cpfA.dig1, a->cpfA.dig2, a->cpfA.dig3, a->cpfA.cod);
 	printf("Telefone: %d\n", a->telefone);
-	printf("Data de Nascimento: %02hd/%02hd/%hd\n", a->nasc.dia, a->nasc.mes, a->nasc.ano);
+	printf("Data de Nascimento: %02d/%02d/%d\n", a->nasc.dia, a->nasc.mes, a->nasc.ano);
 	printf("Endereco: %s, %s, %s, %s - %s\n", a->end.rua, a->end.comp, a->end.bairro, a->end.cidade, a->end.estado);
 	return ALN_CondRetOK;
 }
