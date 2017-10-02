5529e962d3981243ad18cf58afd3bb930c87160d
flaviofvaz <flaviofrancovaz@gmail.com>
2017-10-02 20:23:53 -0300

    Update corpodiscente.h

--- corpodiscente.h
+++ corpodiscente.h
@@ -14,7 +14,7 @@ typedef enum {
 *  $FC Função: CDIS cria
 *
 *  $ED Descrição da função
-*		Cria lista de alunos do corpo discente.
+*		Cria lista de Alunos do Corpo Discente.
 *
 ***********************************************************************/
 CDIS_tpCondRet CorpoDisc_cria (void);
@@ -23,7 +23,7 @@ CDIS_tpCondRet CorpoDisc_cria (void);
 *  $FC Função: CDIS deleta
 *
 *  $ED Descrição da função
-*		Varre a lista do corpo discente procurando Aluno com matrícula igual ao valor de mat1 e o remove da lista.
+*		Varre a lista do Corpo Discente procurando Aluno com matrícula igual ao valor de mat1 e o remove da lista.
 *
 ***********************************************************************/
 CDIS_tpCondRet CorpoDisc_deleta(CorpoDisc *Corpo, unsigned int mat1);
@@ -32,7 +32,15 @@ CDIS_tpCondRet CorpoDisc_deleta(CorpoDisc *Corpo, unsigned int mat1);
 *  $FC Função: CDIS imprime
 *
 *  $ED Descrição da função
-*		Imprime na tela o corpo discente.
+*		Imprime na tela o Corpo Discente.
 *
 ***********************************************************************/
 CDIS_tpCondRet CorpoDisc_imprime (CorpoDisc *Corpo);
+/***********************************************************************
+*
+*  $FC Função: CDIS insere
+*
+*  $ED Descrição da função
+*		Cria e insere Aluno no final da lista do Corpo Discente.
+*
+***********************************************************************/
