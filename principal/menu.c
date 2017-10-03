4416b961deea277c6245a5034338c8dfc2e20353
MzBr <matheuszeitune2@gmail.com>
2017-10-03 19:08:43 -0300

    comentario inicial

--- principal/menu.c
+++ principal/menu.c
@@ -1,3 +1,23 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo professor
+*
+*  Arquivo gerado:              main.C
+*  Letras identificadoras:      PRF
+*
+*  Nome da base de software:    Fonte do módulo principal
+*
+*  Projeto: Disciplina INF 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: Matheus Zeitune, MZ.
+*			
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    			  Data     Observações
+*       1.00   MZ.   2/10/2017 Desenvolvimento para T2 
+*
+*  $ED Descrição do módulo
+*     Este modulo contém o menu.
+***************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- principal/menu.h
+++ principal/menu.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo professor
+*
+*  Arquivo gerado:              main.C
+*  Letras identificadoras:      PRF
+*
+*  Nome da base de software:    Fonte do módulo principal
+*
+*  Projeto: Disciplina INF 1301
+*  Gestor:  DI/PUC-Rio
+*  Autores: Matheus Zeitune, MZ.
+*			
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    			  Data     Observações
+*       1.00   MZ.   2/10/2017 Desenvolvimento para T2 
+*
+*  $ED Descrição do módulo
+*     Este modulo contém o menu
+***************************************************************************/
+
 void MEN_menuInicial(void);
 void MEN_menuAluno(void);
 int MEN_loginAluno(void);
